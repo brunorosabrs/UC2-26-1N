8eed3640b29cedcc51329b000d575c5b3045d451
Bruno <brunorosabrs1234@gmail.com>
2026-04-01 21:44:50 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,43 @@
+const ask = require('readline-sync');
+
+//let name = ask.question("Qual eh o seu nome?");
+//console.log("Olá", name);
+
+//let nome = "Bruno";
+//let idade = 29;
+//let frase = `meu nome eh ${nome} e tenho  ${idade} anos`;
+//console.log(frase);
+
+//let name = ask.question("Qual eh o seu nome? ");
+//let favoriteColor = ask.question("Qual a sua cor favorita? ");
+//console.log(`Meu nome é ${name} e minha cor favorita eh ${favoriteColor}`);
+
+//let name = "Bruno";
+//console.log(name.length);
+
+//let frase = "OieeeEeee";
+//let fraseMinuscula = frase.toLowerCase();
+//console.log(fraseMinuscula);
+
+
+//let frase = "OieeeEeee";
+//let fraseMinuscula = frase.toUpperCase();
+//console.log(fraseMinuscula);
+
+//let email = "    brunorosabrs1234@gmail.com         ";
+//console.log(email.trim());
+
+//let frase = "Hoje comi cenoura";
+//frase.includes("cenoura"); // true
+//frase.includes("batata"); // false
+//console.log(frase);
+
+//let frase = "Hoje comi arroz, feijao e carne";
+//let fraseFiltrada = frase.includes("feijao");
+//console.log(`hoje comi feijao? ${fraseFiltrada}`);
+
+//let frase = "hoje comi cenoura, adoro cenoura";
+///let novaFrase = frase.replaceAll("cenoura", "batata");
+//console.log(novaFrase);
+
+
